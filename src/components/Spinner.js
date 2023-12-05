e8703fba695e1d76abdc9d1e49cf3dc5d3fd0cd8
wtomkarkhude <omkarkhude1@gmail.com>
2023-12-05 22:54:11 +0530

    committed as function based

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
+import loading from './loading.gif'
 
-export class Spinner extends Component {
-  render() {
+const Spinner=()=> {
+  
     return (
+    // <div className="text-center">
+    //     <div className="spinner-border" role="status">
+    //     <span className="visually-hidden">Loading...</span>
+    //     </div>
+    // </div> 
     <div className="text-center">
-        <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
-        </div>
-    </div> 
+                <img className="my-3" src={loading} alt="loading" />
+            </div>
     )
   }
-}
+
 
 export default Spinner
